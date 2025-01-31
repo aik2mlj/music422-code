d7a8ef1a0468e9b1153c4f95e0616e2cabc6bc0b
aik2 <aik2mlj@gmail.com>
2025-01-30 17:13:59 -0800

    [3] 1.a & 1.b

---
+++ Hw3/hw3_lejunmin.typ
@@ -0,0 +1,32 @@
+#import "@preview/problemst:0.1.0": pset
+
+#show: pset.with(
+  class: "MUSIC 422",
+  student: "Lejun Min",
+  title: "Homework 3",
+  date: datetime.today(),
+  subproblems: "1.a.i)",
+)
+
+=
+==
+=== Relation between input and aliased MDCT/IMDCT output
+Each MDCT block is computed over a windowed segment of the input signal with a length of N, but it only produces N/2 frequency-domain coefficients. This results in time-domain aliasing because adjacent blocks overlap by 50%, and the information is distributed across consecutive blocks.
+
+- The MDCT output contains transformed frequency components from a windowed portion of the input, but due to the transform's properties, adjacent blocks share spectral information.
+- The IMDCT output reconstructs an aliased time-domain signal, where each block contains contributions from two overlapping input segments.
+
+=== How overlap-add will recover the input signal
+Each IMDCT block has contributions from two adjacent input blocks, overlapping them and summing cancels the aliasing components due to the design of the TDAC principle, ensuring perfect reconstruction.
+
+==
+=== How to initialize the first block of input samples?
+Extract the first 4 data samples, and prepend them with 4 zeros.
+
+=== What did you need to do to get the last block of samples?
+Append 4 zeros for the last block of samples before feeding into MDCT/IMDCT.
+
+=== Is there any delay in your output signal? How much?
+Yes. There is a delay of 4 data samples.
+
+
--- Hw3/src/mdct.py
+++ Hw3/src/mdct.py
@@ -25,8 +25,20 @@ def MDCTslow(data, a, b, isInverse=False):
     """
 
     ### YOUR CODE STARTS HERE ###
+    N = a + b
+    n0 = (b + 1) / 2
+    n_plus_n0 = np.arange(0, N) + n0
+    k_plus_half = np.arange(0, N // 2) + 0.5
+    cos_matrix = np.cos((2 * np.pi / N) * np.outer(n_plus_n0, k_plus_half))  # (N,N/2)
 
-    return np.zeros_like(data)  # CHANGE THIS
+    if isInverse:
+        summand = cos_matrix * data
+        x = 2 * np.sum(summand, axis=1)
+        return x
+    else:
+        summand = cos_matrix.T * data
+        X = (2 / N) * np.sum(summand, axis=1)
+        return X
     ### YOUR CODE ENDS HERE ###
 
 
@@ -58,7 +70,33 @@ def IMDCT(data, a, b):
 # Testing code
 if __name__ == "__main__":
     ### YOUR TESTING CODE STARTS HERE ###
+    xs = [0, 1, 2, 3, 4, 4, 4, 4, 3, 1, -1, -3]
+    a = 4
+    b = 4
+    N = a + b
+    xs_pad = np.concat((xs, np.zeros(b)))
+    xs_pad = np.concat((np.zeros(a), xs_pad))
+    # print(xs)
 
-    pass  # THIS DOES NOTHING
+    idx = a  # starting from 4
+    last_half_recon = np.zeros(N // 2)
+    output = []
+    while idx + b <= len(xs_pad):
+        x = xs_pad[idx - a : idx + b]
+        print(x)
 
+        mdct_x = MDCTslow(x, a, b, isInverse=False)
+        x_prime = MDCTslow(mdct_x, a, b, isInverse=True)
+        x_prime /= 2.0
+        x_recon = last_half_recon + x_prime[: N // 2]
+        last_half_recon = x_prime[N // 2 :]
+        if np.allclose(x_recon, x[: N // 2], atol=1e-10):
+            print("reconstruction success!")
+        else:
+            print("recon:", x_recon)
+            print("wrong!")
+        output.append(x_recon)
+        idx += b
+    output = np.concat(output[1:])
+    assert np.allclose(output, xs, atol=1e-10)
     ### YOUR TESTING CODE ENDS HERE ###
